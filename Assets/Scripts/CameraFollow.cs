61cb086baf6a0493e3a010c93d972a046c6edc8e
BartDrown <bjakubowski.dev@gmail.com>
2020-10-02 22:28:04 +0200

    git init

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float smoothSpeed = 0.125f;
+
+    [SerializeField]
+    Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate() {
+
+        Vector3 targetPosition = target.TransformPoint(offset);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
+    }
+    
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    [SerializeField]
+    float speed = 1f;
+
+    float verticalMove = 0f;
+    float horizontalMove = 0f;
+
+    CharacterController character;
+
+    void Start() {
+        this.gameObject.AddComponent<CharacterController>();
+        character = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        verticalMove = Input.GetAxisRaw("Vertical") * speed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    void FixedUpdate() {
+
+
+        character.Move(new Vector3(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime, 0));
+
+    }
+}
